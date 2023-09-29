4f58154cc2b6ac9619a87119e6471d1e05c8b21a
Daniel <danielstonehalifax@gmail.com>
2023-09-29 10:30:28 -0300

    fixed it in the build

--- Assets/Scripts/WaterWheelRotator.cs
+++ Assets/Scripts/WaterWheelRotator.cs
@@ -13,6 +13,6 @@ public class WaterWheelRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.2f, 0 * Time.deltaTime);
+        transform.Rotate(0, 1, 0 * Time.deltaTime);
     }
 }
--- Assets/Scripts/WindmillRotator.cs
+++ Assets/Scripts/WindmillRotator.cs
@@ -13,7 +13,7 @@ public class WindmillRotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.1f, 0 * Time.deltaTime);
+        transform.Rotate(0, 1.5f, 0 * Time.deltaTime);
 
     }
 }
